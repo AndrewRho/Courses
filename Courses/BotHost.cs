4f58c4c06868754d221fd56824cd3c96128061ab
AndrewRho <AndreiRogov@ukr.net>
2024-09-25 21:32:43 +0300

    chore: added swagger and empty bot host for testing

---
+++ Courses/BotHost.cs
@@ -0,0 +1,60 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Courses;
+
+public class BotHost
+{
+    private readonly string _token;
+    private readonly TelegramBotClient _client;
+
+    public BotHost(string token)
+    {
+        _token = token;
+        _client = new TelegramBotClient(token);
+    }
+
+    public void Start()
+    {
+        _client.StartReceiving(UpdateHandler, ErrorHandler);
+        
+    }
+
+    private async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken token)
+    {
+        if (update.Type == UpdateType.CallbackQuery)
+        {
+            await client.SendTextMessageAsync(
+                update.CallbackQuery.Message.Chat.Id,
+                "Got it",
+                cancellationToken: token);
+            return;
+        }
+        
+        
+        InlineKeyboardMarkup inlineKeyboard = new( new[]
+        {
+            // first row
+            new[]
+            {
+                InlineKeyboardButton.WithCallbackData(text: "Button 1", callbackData: "callback 1"),
+                InlineKeyboardButton.WithCallbackData(text: "Button 2", callbackData: "callback 2"),
+            },
+ 
+        });
+        
+        
+        await client.SendTextMessageAsync(
+            update.Message.Chat.Id,
+            "Buttons",
+            replyMarkup: inlineKeyboard,
+            cancellationToken: token);
+    }
+
+    private async Task ErrorHandler(ITelegramBotClient client, Exception ex, CancellationToken token )
+    {
+        var q = 0;
+    }
+}
--- Courses/Program.cs
+++ Courses/Program.cs
@@ -1,8 +1,18 @@
+using Courses;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
 var app = builder.Build();
 app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseEndpoints(e => e.MapControllers());
+
+var host = new BotHost("***");
+host.Start();
+
+
 app.Run();
